931383b269ca9e23d5227cc5a738d7fba9269686
wagner tavares <wagnertavares2010@gmail.com>
2022-12-08 18:27:36 -0300

    second commit

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express,{Request , Response} from 'express';
 import path from 'path';
+import mustache from 'mustache-express';
+
 import mainRoutes from './routes';
 
 const server = express();
+
+server.set('view engine' , 'mustache');
+server.set('views' , path.join(__dirname, 'views'));
+server.engine('mustache' , mustache());
+
 
 server.use('/static',express.static(path.join(__dirname,'../public')));
 
